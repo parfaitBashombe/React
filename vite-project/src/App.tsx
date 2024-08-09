1f1c54baf570ddeddfbca9ca804f21e0ffd09889
Parfait Bashombe <parfaitbashombe@gmail.com>
2024-08-09 16:44:41 +0300

    modif 1

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,18 @@
+import Heading from "./components/Heading";
+import Section from "./components/Section";
+import Array from "./components/Array";
+
+function App() {
+  return (
+    <>
+      {" "}
+      <Heading title={"Lorem ipsum "} />
+      <Section>
+        {"lorem ipsum santo do caprio ce catrio sion spaghtti."}
+      </Section>
+      <Array></Array>
+    </>
+  );
+}
+
+export default App;
